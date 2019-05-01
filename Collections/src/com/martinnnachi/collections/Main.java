90332f7c87d7fa7e1bb3c40c1a9dd9087d001d83
Martin Nnachi <mnnachi@gmail.com>
2019-05-01 15:32:45 +0100

    First commit

---
+++ Collections/src/com/martinnnachi/collections/Main.java
@@ -0,0 +1,19 @@
+package com.martinnnachi.collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Theatre theatre = new Theatre("Royal Alberts Hall", 8, 12);
+	theatre.getSeats();
+	if (theatre.reserveSeat("H11")){
+        System.out.println("Please pay");
+    }else {
+        System.out.println("Sorry, seat is taken");
+    }
+	if (theatre.reserveSeat("H11")){
+        System.out.println("Please pay");
+    }else {
+        System.out.println("Sorry, seat is taken");
+    }
+    }
+}
